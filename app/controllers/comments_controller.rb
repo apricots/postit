a97c1ce27a6e940f8ace9836b129cca46d612ed7
apricots <potatonisha@gmail.com>
2014-03-30 00:29:37 -0400

    new features

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.creator = User.first
+    @comment.post = @post
+    if @comment.save
+      flash[:notice] = "Comment submitted."
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
