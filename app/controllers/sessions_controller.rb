88700733350822efaae331a32c84f2c33308071e
apricots <potatonisha@gmail.com>
2014-05-05 22:14:30 -0400

    twilio two-factor auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by username: (params[:username])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:notice] = "Hi, #{user.username}. You have been logged in."
-      redirect_to root_path
+      #determine if 2factor authentication is on
+      if user.two_factor_auth?
+        # restrict access to pin_path via session parameters
+        session[:two_factor] = true
+        # gen a pin, send pin to twilio, sms to user's phone
+        user.generate_pin!
+        user.send_pin_to_twilio
+        # show pin form
+        redirect_to pin_path
+      else
+        login_user!(user)
+      end
     else
       flash[:error] = "There was an error in your username and password combination. Please retry."
       redirect_to login_path
@@ -20,4 +29,28 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  def pin
+    access_denied if session[:two_factor].nil?
+    #renders pin template automatically if it's not a request
+    if request.post?
+      user = User.find_by pin: params[:pin]
+      #success condition
+      if user
+        session[:two_factor] = nil
+        user.remove_pin!
+        #remove the pin
+        #log user in
+        login_user!(user)
+      else
+        flash[:error] = "Sorry, something is wrong with your pin number."
+      end
+    end
+  end
+
+  private
+    def login_user!(user)
+      session[:user_id] = user.id
+      flash[:notice] = "Hi, #{user.username}. You have been logged in."
+      redirect_to root_path
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,40 @@ class User < ActiveRecord::Base;
   validates :username, presence: true, length: {minimum: 3}
   validates :password, presence: true, on: :create, length: {minimum: 5}
   validates_uniqueness_of :username, :case_sensitive => false
+  validates :phone, length: {is: 10}, numericality: { only_integer: true }, allow_blank: true
 
   sluggable_column :username
 
   def admin?
     self.role == 'admin'
+  end
+
+  def two_factor_auth?
+    !self.phone.blank?
+  end
+
+  # generates random 6-digit number and automatically hits db
+  def generate_pin!
+    self.update_column(:pin, rand(10 ** 6))
+  end
+
+  def remove_pin!
+    self.update_column(:pin, nil)
+  end
+
+  def send_pin_to_twilio
+    # put your own credentials here 
+    account_sid = 'AC86d6066bfd9a5be5705c00e88b175818' 
+    auth_token = '423bc7ab833fe9e1987352e941253b31' 
+
+    # set up a client to talk to the Twilio REST API 
+    client = Twilio::REST::Client.new account_sid, auth_token 
+    number = self.phone
+    msg = "Hi. Please input the pin to continue login. Pin: #{self.pin}"
+    client.account.messages.create({
+    :from => '+16467914343', 
+    :to => number, 
+    :body => msg,  
+    })
   end
 end
